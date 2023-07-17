040bca975e9f507805465555ae5ec4d6daa29271
CarlosCalMen <113527119+CarlosCalMen@users.noreply.github.com>
2023-07-16 22:11:11 -0500

    models done

---
+++ api/src/models/Genre.js
@@ -0,0 +1,19 @@
+const {DataTypes} = require ('sequelize');
+
+module.exports = (sequelize)=>{
+    sequelize.define('Genres',{
+        id:{
+            type:DataTypes.INTEGER,
+            primaryKey:true,
+            autoIncrement:true,
+        },
+        name:{
+            type:DataTypes.STRING,
+            allowNull:false,
+            unique:true,
+            validate:{
+                isNull: {msg:'Name could not be null'}
+            }
+        }
+    },{timestamps:false})
+};
--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -3,10 +3,72 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('videogame', {
+  sequelize.define('Videogame', {
+    id:{
+      type:DataTypes.UUID,
+      primaryKey:true,
+      defaultValue:DataTypes.UUIDV1,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique:true,
+      validate:{
+        notEmpty:{
+          msg:'Name could not be empty,'
+        }
+      },
     },
-  });
+    description:{
+      type:DataTypes.STRING,
+      allowNull: false,
+      validate:{
+        notEmpty:{
+          msg:'Description could not be empty'
+        },
+      }
+    },
+    platforms:{
+      type:DataTypes.ARRAY(DataTypes.STRING),
+      allowNull: false,
+      validate:{
+        hasAtLeastOnePlatform(value){
+          if (!value.length)
+            throw new Error ('You must indicate at leats one plataform for the game')
+        }
+      }
+    },
+    image:{
+      type:DataTypes.STRING,
+      allowNull:false,
+      valdate:{
+        isUrl:{
+          args:true,
+          msg:'Image must be a valid URL string',
+        }
+      }
+    },
+    releaseDate:{
+      type:DataTypes.DATEONLY,
+      allowNull:false,
+      validate: {
+        isValidDate(value) {
+          if (!(value instanceof Date))
+            throw new Error('Release date must be a valid date');
+        }
+      }
+    },
+    rating:{
+      type:DataTypes.DECIMAL(2,1),
+      allowNull:false,
+      validate:{
+        max:5.0,
+        min:1.0
+      }
+    },
+    created:{
+      type:DataTypes.BOOLEAN,
+      default:true,
+    }
+  }, {timestamp:false});
 };
